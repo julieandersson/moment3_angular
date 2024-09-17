c50006545d776a89ea8ef84efd35fe5ae2b71504
julieandersson <juan2301@student.miun.se>
2024-09-17 17:55:16 +0200

    skapar en ny component convert för konvertering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ConvertComponent } from './convert/convert.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent},
-    { path: "about", component: AboutComponent}
+    { path: "about", component: AboutComponent},
+    { path: "convert", component: ConvertComponent}
 ];
