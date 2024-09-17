edaceadeb407086d635c2a42b229ea6fe8c89ade
julieandersson <juan2301@student.miun.se>
2024-09-17 18:06:55 +0200

    skapar en notfoundcomponent för felaktigt anrop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ConvertComponent } from './convert/convert.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
     { path: "home", component: HomeComponent},
     { path: "about", component: AboutComponent},
     { path: "convert", component: ConvertComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: "404", component: NotfoundComponent},
+    { path: "**", component: NotfoundComponent}
 ];
